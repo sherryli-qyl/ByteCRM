5aaf131bef5285546334bff6386e6e9d27fd737f
yyr19921203 <123775172@qq.com>
2020-09-11 22:57:54 +1000

    change route

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -15,12 +15,7 @@ const Routes = () => (
         <Route exact path="/companies" component={CompanyList} />
         {/* <Route exact path="/logout" component={Logout} />
         <Route exact path="/register" component={RegForm} /> */}
-<<<<<<< HEAD
-        {/* others' change */}
-
-=======
-        {/* my change */}
->>>>>>> emailcomponents
+        
     </Switch>
    );
 
