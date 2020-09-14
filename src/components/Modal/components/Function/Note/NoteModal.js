fd0a53ce03bd60261a23fed5181004807156b3f2
DerrickSong <sbz7drk@gmail.com>
2020-09-14 21:13:14 +1000

    add code in Call

--- src/components/Modal/components/Function/Note/NoteModal.js
+++ src/components/Modal/components/Function/Note/NoteModal.js
@@ -4,20 +4,16 @@ import './components/RichTextInput';
 import RichTextInput from './components/RichTextInput';
 import NoteSaveBar from './components/NoteSaveBar';
 
-
 const NoteModal = () => (
   <section id="NoteModal" className="NoteModal">
     <div className="note-input">
-      <RichTextInput 
+      <RichTextInput
         className="note-input"
         placeholder="Please start typing note..."
       />
     </div>
-      <NoteSaveBar 
-        className="note-container-footer"
-      />
+    <NoteSaveBar className="note-container-footer" />
   </section>
-  );
-
+);
 
 export default NoteModal;
