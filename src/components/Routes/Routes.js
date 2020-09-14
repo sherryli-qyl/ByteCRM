8310b37399d209cbd0b679f8dc301a623dafa2f4
cathyyfyang <51479638+cathyyfyang@users.noreply.github.com>
2020-09-14 18:44:53 +1000

    Revert "add company basic page"

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -3,7 +3,6 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import ContactList from '../Contact/ContactList';
 import ContactMain from '../Contact/ContactMain';
 import CompanyList from '../Company/CompanyList';
-import CompanyMain from '../Company/CompanyMain';
 import Logout from '../Log/RegForm/Register';
 import RegForm from '../Log/RegForm/Register';
 import LoginForm from '../Log/LoginForm/Login';
@@ -15,7 +14,6 @@ const Routes = () => (
     <Route exact path="/contacts" component={ContactList} />
     <Route exact path="/contacts/main" component={ContactMain} />
     <Route exact path="/companies" component={CompanyList} />
-    <Route exact path="/companies/main" component={CompanyMain} />
     <Route exact path="/logout" component={Logout} />
     <Route exact path="/register" component={RegForm} />
   </Switch>
