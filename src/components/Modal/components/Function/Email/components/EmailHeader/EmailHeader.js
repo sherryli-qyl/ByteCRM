dc8ad333b39846f01c18e61ba11529f9c0b8dc94
cathyyfy <71114498+cathyyfy@users.noreply.github.com>
2020-09-15 13:55:17 +1000

    Revert "Revert "update Editor & Email""

--- src/components/Modal/components/Function/Email/components/EmailHeader/EmailHeader.js
+++ src/components/Modal/components/Function/Email/components/EmailHeader/EmailHeader.js
@@ -9,22 +9,53 @@ import Text from '../../../../../../Style/Text';
 class EmailHeader extends React.Component {
     constructor(props) {
         super(props);
-
         this.state = {
             ccActive: false,
+            emailReceivers: this.props.contacts,
+            carbonCopies: this.props.contacts,
         }
         this.onCcButtonClick = this.onCcButtonClick.bind(this);
+        this.onDeleteReceiversButton = this.onDeleteReceiversButton.bind(this);
+        this.onDeleteCCsButton = this.onDeleteCCsButton.bind(this);
     }
 
-    onCcButtonClick(){
+    onCcButtonClick() {
         this.setState({
-            ccActive:true,
+            ccActive: true,
         })
     }
 
+    onDeleteReceiversButton(canceledemail) {
+        console.log(canceledemail);
+        let newContacts = this.state.emailReceivers;
+        for (let key in newContacts) {
+            if (newContacts[key].email === canceledemail) {
+                newContacts.splice(key, 1)
+            }
+        }
+        this.setState({
+            emailReceivers: newContacts,
+        });
+        console.log("length" + this.props.contacts.length);
+    }
+
+    onDeleteCCsButton(canceledemail) {
+        console.log(canceledemail);
+        let newContacts = this.state.carbonCopies;
+        for (let key in newContacts) {
+            if (newContacts[key].email === canceledemail) {
+                newContacts.splice(key, 1)
+            }
+        }
+        this.setState({
+            carbonCopies: newContacts,
+        });
+    }
+
+
 
     render() {
-        const { ccActive } = this.state;
+        const { ccActive, emailReceivers, carbonCopies } = this.state;
         return (
             <div className="emailHeader">
                 <div className="emailHeader__blank" />
@@ -33,16 +64,22 @@ class EmailHeader extends React.Component {
                         value={"To"}
                         borderBottom={false}
                     >
-                        <ReceiverTag>John  Wick</ReceiverTag>
-                        <ReceiverTag>John 111111  Wick</ReceiverTag>
+                        {emailReceivers.map((item) => (
+                            <ReceiverTag onClick={() => this.onDeleteReceiversButton(item.email)}>
+                                {item.value}
+                            </ReceiverTag>
+                        ))}
                     </HeaderItem>
                     {ccActive ?
                         <HeaderItem
                             value={"Cc"}
                             borderBottom={false}
                         >
-                            <ReceiverTag>John  Wick</ReceiverTag>
-                            <ReceiverTag>John 111111  Wick</ReceiverTag>
+                            {carbonCopies.map((item) => (
+                                <ReceiverTag onClick={() => this.onDeleteCCsButton(item.email)}>
+                                    {item.value}
+                                </ReceiverTag>
+                            ))}
                         </HeaderItem>
                         :
                         <div />
@@ -55,26 +92,28 @@ class EmailHeader extends React.Component {
                             <span>abc</span>
                         </div>
                         <div className={
-                            ccActive?
-                             "item-content__right" 
-                             : 
-                             "item-content__right item-content__right__active"
-                             }>
+                            ccActive ?
+                                "item-content__right"
+                                :
+                                "item-content__right item-content__right__active"
+                        }>
                             <button className="ccButton"
-                            onClick={(event) => {
-                                event.preventDefault();
-                                this.onCcButtonClick();}}
-                                >
-                            <Text className="ccButton__text">Cc</Text>
+                                onClick={(event) => {
+                                    event.preventDefault();
+                                    this.onCcButtonClick();
+                                }}
+                            >
+                                <Text className="ccButton__text">Cc</Text>
                             </button>
                         </div>
                     </HeaderItem>
                     <HeaderItem
                         value={"Subject"}
                         borderBottom={true}
+                        extraClassName={"subject"}
                     >
-                        <div className="item-content subject">
-                            <span>abc</span>
+                        <div className="item-content">
+                            <input className='subject__input' />
                         </div>
                     </HeaderItem>
                 </div>
