9cdd79c46db5e34dc8e0acae4becbe86aee667e3
yyr19921203 <123775172@qq.com>
2020-09-15 20:25:39 +1000

    fix bugs and missing files

---
+++ src/components/Contact/ContactMain/components/Activities/Activities.js
@@ -0,0 +1,60 @@
+import React, { Fragment } from 'react';
+import Container from '@material-ui/core/Container';
+
+import TabBar from './components/TabBar';
+import './Activities.scss'
+
+
+class Activities extends React.Component {
+    constructor(props) {
+        super(props);
+        const tabItems = [
+            { key: 'Activity', value: 'Activity', activity: (<div>Activity</div>) },
+            { key: 'Notes', value: 'Notes', activity: (<div>Notes</div>) },
+            { key: 'Emails', value: 'Emails', activity: (<div>Emails</div>) },
+            { key: 'Calls', value: 'Calls', activity: (<div>Calls</div>) },
+            { key: 'Tasks', value: 'Tasks', activity: (<div>Tasks</div>) },
+            { key: 'Meetings', value: 'Meetings', activity: (<div>Meetings</div>) },
+        ];
+        this.state = {
+            currentActivity: tabItems[0].key,
+            tabItems,
+        }
+        this.handleOnclick = this.handleOnclick.bind(this);
+    }
+
+    handleOnclick(item){
+        console.log(item.key);
+       this.setState({
+           currentActivity:item.key,
+       })
+    }
+
+    render() {
+        const { tabItems, currentActivity } = this.state;
+
+        return (
+            <div className='container'>
+                <TabBar
+                    tabItems={tabItems}
+                    currentPage={currentActivity}
+                    onTabItemClick={this.handleOnclick}
+                />
+                <div className="activityPage">
+                    {tabItems.map((tabItem) => {
+                        if (currentActivity !== tabItem.key) {
+                            return null;
+                        }
+                        return (
+                            <React.Fragment key={tabItem.key}>
+                                {tabItem.activity}
+                            </React.Fragment>
+                        );
+                    })}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Activities;
---
+++ src/components/Contact/ContactMain/components/Activities/components/TabBar/TabBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import TabItem from './TabItem/TabItem';
+
+import './TabBar.scss';
+
+const TabBar = ({
+  tabItems,
+  currentPage,
+  onTabItemClick,
+}) => (
+    <nav className="tabBar">
+      {tabItems.map((item) => (
+      <TabItem 
+        key={item.key}
+        active={currentPage === item.key} 
+        onClick={() => onTabItemClick(item)}
+      >
+        {item.value}
+      </TabItem>
+    ))}
+  </nav>
+);
+
+export default TabBar;
