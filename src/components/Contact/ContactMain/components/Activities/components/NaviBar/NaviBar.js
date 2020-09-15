c70c3afda8dfa28da136dffc3ef22c61ec260487
yyr19921203 <123775172@qq.com>
2020-09-15 18:41:00 +1000

    commit

--- src/components/Contact/ContactMain/components/Activities/components/NaviBar/NaviBar.js
+++ src/components/Contact/ContactMain/components/Activities/components/NaviBar/NaviBar.js
@@ -6,6 +6,7 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import "./NaviBar.scss";
 
 
 function TabPanel(props) {
@@ -58,7 +59,7 @@ export default function NaviBar() {
   };
 
   return (
-    <div className={classes.root}>
+    <div className={classes.root + "activitiesNav"}>
       <AppBar position="static" color="default">
         <Tabs
           value={value}
