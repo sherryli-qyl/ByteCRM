3c70f94c4df6804b81b6e6e2ca9598199cda3454
yyr19921203 <123775172@qq.com>
2020-09-15 20:13:02 +1000

    commit

--- src/components/Contact/ContactMain/components/Activities/Activities.js
+++ src/components/Contact/ContactMain/components/Activities/Activities.js
@@ -1,16 +1,15 @@
 import React,{Fragment} from 'react';
 import Container from '@material-ui/core/Container';
-
+import "./Activities.scss";
 import NaviBar from './components/NaviBar/NaviBar';
 const Activities =()=>{
 
     return(
-        <Fragment>
+       <div className='activitiesPage'>
         <Container maxWidth="md">
           <NaviBar />
-  
         </Container>
-    </Fragment>
+        </div>
     )
 }
 
--- src/components/Contact/ContactMain/components/Activities/components/NaviBar/NaviBar.js
+++ src/components/Contact/ContactMain/components/Activities/components/NaviBar/NaviBar.js
@@ -1,39 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import { ThemeProvider } from '@material-ui/core/styles';
+import TabPanel from './components/TabPanel';
+import getTheme from '../../../../../../Style/Theme/MatUITheme';
+
 import "./NaviBar.scss";
 
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`scrollable-auto-tabpanel-${index}`}
-      aria-labelledby={`scrollable-auto-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
 
 function a11yProps(index) {
   return {
@@ -42,58 +18,64 @@ function a11yProps(index) {
   };
 }
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    width: '100%',
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
+class NaviBar extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default function NaviBar() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+    const tabBarItems = [
+      { key: 0, value: "Activity", page: (<div>Activity</div>) },
+      { key: 1, value: "Notes", page: (<div>Notes</div>) },
+      { key: 2, value: "Emails", page: (<div>Emails</div>) },
+      { key: 3, value: "Calls", page: (<div>Calls</div>) },
+      { key: 4, value: "Tasks", page: (<div>Tasks</div>) },
+      { key: 5, value: "Meetings", page: (<div>Meetings</div>) },
+    ]
+    const theme = getTheme();
+    this.state = {
+      tabBarItems,
+      value: "",
+      theme
+    }
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (event, newValue) => {
+    this.setState({
+      value: newValue,
+    })
   };
 
-  return (
-    <div className={classes.root + "activitiesNav"}>
-      <AppBar position="static" color="default">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="scrollable"
-          scrollButtons="auto"
-          aria-label="scrollable auto tabs example"
-        >
-          <Tab label="Activity" {...a11yProps(0)} />
-          <Tab label="Notes" {...a11yProps(1)} />
-          <Tab label="Emails" {...a11yProps(2)} />
-          <Tab label="Calls" {...a11yProps(3)} />
-          <Tab label="Meetings" {...a11yProps(4)} />
-          <Tab label="6" {...a11yProps(5)} />
-          <Tab label="7" {...a11yProps(6)} />
-        </Tabs>
-      </AppBar>
-      <TabPanel value={value} index={0}>
-    
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-          </TabPanel>
-      <TabPanel value={value} index={5}>
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-      </TabPanel>
-    </div>
-  );
+
+  render() {
+    const { value, tabBarItems,theme } = this.state;
+    return (
+      <div>
+        <ThemeProvider theme={theme}>
+          <AppBar position="static" color="default">
+            <Tabs
+              value={value}
+              onChange={this.handleChange}
+              indicatorColor="primary"
+              textColor="primary"
+              variant="scrollable"
+              scrollButtons="auto"
+              aria-label="scrollable auto tabs example"
+            >
+              {tabBarItems.map((item) => (
+                <Tab label={item.value} {...a11yProps(item.key)} />
+              ))}
+            </Tabs>
+          </AppBar>
+          {tabBarItems.map((item) => (
+            <TabPanel value={value} index={item.key}>
+              {item.page}
+            </TabPanel>
+          ))}
+        </ThemeProvider>
+      </div >
+    );
+  }
 }
+
+export default NaviBar;
