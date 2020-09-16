6e81c77d4b74d60fa44e48cfc064e93ede0e2e02
Anna <anna.dai@student.adelaide.edu.au>
2020-09-16 18:44:08 +0930

    reformat about company component

--- src/components/Company/CompanyMain/components/Basic/components/AboutCompany/AboutCompany.js
+++ src/components/Company/CompanyMain/components/Basic/components/AboutCompany/AboutCompany.js
@@ -3,20 +3,20 @@ import CompanyDetail from "./components/CompanyDetail"
 import "./AboutCompany.scss";
 
 const companyInfo = [
-	{ label: "Name", input: "HubSpot, Inc." },
-	{ label: "Company domain name", input: "hubspot.com" },
-	{ label: "Industry", input: "Computer Software" },
-	{ label: "Phone number", input: "+1 888-482-7768." },
-	{ label: "Company owner", input: "No owner" },
-	{ label: "Type", input: " " },
-	{ label: "City", input: "Cambridge" },
-	{ label: "State/Region", input: "MA" },
-	{ label: "Postal code", input: "02141" },
-	{ label: "Number of employees", input: "5,000" },
-	{ label: "Annual revenue", input: "$250,000,000.00" },
-	{ label: "Time zone", input: "America/New_York" },
-	{ label: "Description", input: "HubSpot is an American developer and marketer of software products for inbound marketing, sales, and customer service.1" },
-	{ label: "LinkedIn company page", input: "https://www.linkedin.com/company/hubspotk" }
+	{ key: "Company domain name", input: "hubspot.com" },
+	{ key: "Industry", input: "Computer Software" },
+	{ key: "Phone number", input: "+1 888-482-7768." },
+	{ key: "Company owner", input: "No owner" },
+	{ key: "Type", input: " " },
+	{ key: "City", input: "Cambridge" },
+	{ key: "State/Region", input: "MA" },
+	{ key: "Postal code", input: "02141" },
+	{ key: "Number of employees", input: "5,000" },
+	{ key: "Annual revenue", input: "$250,000,000.00" },
+	{ key: "Time zone", input: "America/New_York" },
+	{ key: "Description", input: "HubSpot is an American developer and marketer of software products for inbound marketing, sales, and customer service.1" },
+	{ key: "LinkedIn company page", input: "https://www.linkedin.com/company/hubspotk" }
+	{ key: "Name", input: "HubSpot, Inc." },
 ]
 
 const AboutCompany = () => (
@@ -24,7 +24,8 @@ const AboutCompany = () => (
 		<h3> About this company</h3>
 		{companyInfo.map((item) => (
 			<CompanyDetail
-				label={item.label}
+				key={item.key}
+				label={item.key}
 				input={item.input}
 			/>
 		))}
