12091bb55a62c073ff8acf65c43a07fbc1c35bd6
yyr19921203 <123775172@qq.com>
2020-09-20 04:22:20 +1000

    build user icon

--- src/components/Contact/ContactMain/components/Activities/components/Functions/Email/components/EmailCards/components/LogEmailCard/LogEmailcard.js
+++ src/components/Contact/ContactMain/components/Activities/components/Functions/Email/components/EmailCards/components/LogEmailCard/LogEmailcard.js
@@ -1,22 +1,39 @@
 import React from 'react';
+import UserTag from '../../../../../../../../../../../Style/Tag/Activity/User';
+import UserIcon from '../../../../../../../../../../../../img/Contact/profile.png';
 import './LogEmailCard.scss';
 
 
 class LogEmailCard extends React.Component {
     constructor(props) {
         super(props);
+        this.Name = "Yurun Yu";
         this.state = {
+            userName: this.Name,
         }
     }
 
     render() {
+        const { userName } = this.state;
         return (
             <div className="logEmailCard">
-                {this.props.date}
-                <div>test logEmail Card</div>
-                <div>test logEmail Card</div>
-                <div>test logEmail Card</div>
-                <div>test logEmail Card</div>
+                <div className='logEmailCard__content'>
+                    {this.props.date}
+                    <div>test logEmail Card</div>
+                    <div>test logEmail Card</div>
+                    <div>test logEmail Card</div>
+                    <div>test logEmail Card</div>
+                </div>
+                <div className='logEmailCard__footer'>
+                    <UserTag
+                        userName={userName}
+                        userIcon={UserIcon}
+                    />
+                    <div className='logEmailCard__footer__label'>
+                        logged an Email
+                    </div>
+                </div>
+
             </div>
         )
     }
