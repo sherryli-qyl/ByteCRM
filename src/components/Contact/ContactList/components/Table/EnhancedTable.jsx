f5c3f75e7258d5d8309c6a07d24bf6c2d75f45ee
Bradley <liu770807152@hotmail.com>
2020-09-22 00:41:19 +1000

    start to work on editor

--- src/components/Contact/ContactList/components/Table/EnhancedTable.jsx
+++ src/components/Contact/ContactList/components/Table/EnhancedTable.jsx
@@ -2,13 +2,13 @@ import React, { Component, useState } from 'react';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
 import MaterialTable from 'material-table';
+import Editor from '../Editor';
+import tableIcons from '../../services/getIcons';
 import getColumns from '../../services/getColumns';
-import TableIcons from '../../services/getIcons';
 import remove from '../../services/removeSelected';
 import exportCSV from '../../services/exportCSV';
 import exportPDF from '../../services/exportPDF';
 import updateRow from '../../services/updateRow';
-
 
 
 // 表格部分样式
@@ -28,73 +28,78 @@ function EnhancedTable(props) {
     const [columns, setColumns] = useState(getColumns());
     const [data, setData] = useState(props.data);
     const [selectedRow, setSelectedRow] = useState(null);
+    const [visible] = useState(false);
+
     return (
-      <MuiThemeProvider theme={Theme}>
-        <MaterialTable
-          title={null}
-          columns={columns}
-          data={data}
-          icons={TableIcons}
-          onRowClick={(evt, selectedRow) => {}}
-          // onSelectionChange={(Rows) => console.log(Rows)}
-          actions={[
-            { tooltip: 'Remove All Selected Contacts',
-              icon: TableIcons.Delete,
-              onClick: (evt, selectedRow) => 
+      <>
+        <Editor visible={visible}></Editor>
+        <MuiThemeProvider theme={Theme}>
+          <MaterialTable
+            title={null}
+            columns={columns}
+            data={data}
+            icons={tableIcons}
+            onRowClick={(evt, selectedRow) => {}}
+            // onSelectionChange={(Rows) => console.log(Rows)}
+            actions={[
+              { tooltip: 'Remove All Selected Contacts',
+                icon: tableIcons.Delete,
+                onClick: (evt, selectedRow) => 
+                  new Promise((resolve, reject) => {
+                      setTimeout(() => {
+                        let dataDelete = [...data];
+                        dataDelete = remove(dataDelete, selectedRow);
+                        setData([...dataDelete]);
+                        resolve();
+                      }, 500);
+                  })
+              }, {
+                tooltip: 'Edit contact',
+                icon: tableIcons.Edit,
+                onClick: (event, rowData) => {
+                  console.log(rowData)
+
+                }
+            }]}
+            options={{
+              selection: true,
+              filtering: false,
+              search: true,
+              sorting: true,
+              pageSize: 10,
+              pageSizeOptions: [10, 30, 50],
+              exportButton: true,
+                exportCsv: (columns, data) => {
+                  exportCSV(columns, data);
+                },
+                exportPdf: (columns, data) => {
+                  exportPDF(columns, data);
+                },
+            }}
+            editable={{
+              onRowAdd: newData =>
                 new Promise((resolve, reject) => {
-                    setTimeout(() => {
-                      let dataDelete = [...data];
-                      dataDelete = remove(dataDelete, selectedRow);
-                      setData([...dataDelete]);
-                      resolve();
-                    }, 500);
-                })
-            }, {
-              tooltip: 'Edit contact',
-              icon: TableIcons.Edit,
-              onClick: (event, rowData) => {
-                console.log(rowData)
-                // TODO1: 需要一个弹窗输入信息
-              }
-          }]}
-          options={{
-            selection: true,
-            filtering: false,
-            search: true,
-            sorting: true,
-            pageSize: 10,
-            pageSizeOptions: [10, 30, 50],
-            exportButton: true,
-              exportCsv: (columns, data) => {
-                exportCSV(columns, data);
-              },
-              exportPdf: (columns, data) => {
-                exportPDF(columns, data);
-              },
-          }}
-          editable={{
-            onRowAdd: newData =>
-              new Promise((resolve, reject) => {
-                newData = updateRow(newData, true);
-                setTimeout(() => {
-                  setData([...data, newData]);
-                  resolve();
-                }, 500);
-              }),
-            onRowUpdate: (newData, oldData) =>
-              new Promise((resolve, reject) => {
-                newData = updateRow(newData);
-                setTimeout(() => {
-                  const dataUpdate = [...data];
-                  const index = oldData.tableData.id;
-                  dataUpdate[index] = newData;
-                  setData([...dataUpdate]);
-                  resolve();
-                }, 500)
-              }),
-          }}
-        />
-      </MuiThemeProvider>
+                  newData = updateRow(newData, true);
+                  setTimeout(() => {
+                    setData([...data, newData]);
+                    resolve();
+                  }, 500);
+                }),
+              onRowUpdate: (newData, oldData) =>
+                new Promise((resolve, reject) => {
+                  newData = updateRow(newData);
+                  setTimeout(() => {
+                    const dataUpdate = [...data];
+                    const index = oldData.tableData.id;
+                    dataUpdate[index] = newData;
+                    setData([...dataUpdate]);
+                    resolve();
+                  }, 500)
+                }),
+            }}
+          />
+        </MuiThemeProvider>
+      </>
     )
 }
 
--- src/components/Contact/ContactList/services/getIcons.js
+++ src/components/Contact/ContactList/services/getIcons.js
@@ -17,7 +17,7 @@ import ViewColumn from '@material-ui/icons/ViewColumn';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 
-const TableIcons = {
+const tableIcons = {
     Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
     Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
     Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
@@ -39,4 +39,4 @@ const TableIcons = {
 };
 
 
-export default TableIcons;
+export default tableIcons;
