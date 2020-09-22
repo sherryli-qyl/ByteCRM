a9d2acbb208d3f5c2eda3f074c01c7379694e5b8
Anna <anna.dai@student.adelaide.edu.au>
2020-09-22 15:43:27 +0930

    add info icon

--- src/components/Company/CompanyMain/components/Basic/components/AboutCompany/components/CompanyDetail.js
+++ src/components/Company/CompanyMain/components/Basic/components/AboutCompany/components/CompanyDetail.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import "./CompanyDetail.scss"
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import "./CompanyDetail.scss";
+// import { info } from 'node-sass';
 
 const CompanyDetail = (props) => (
   <div>
+    <div className ="labelwithinfo">
     <div className="label"> {props.label} </div>
+    <div className="info">
+    <FontAwesomeIcon icon={faInfoCircle} />
+    </div>
+    </div>
     <div className="input"> {props.input} </div>
   </div>
 );
