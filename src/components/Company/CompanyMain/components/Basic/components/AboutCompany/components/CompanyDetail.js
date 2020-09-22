cdfa4539860c02ab9409bd1b535472321129ccd7
Anna <anna.dai@student.adelaide.edu.au>
2020-09-22 17:07:51 +0930

    add tooltip

--- src/components/Company/CompanyMain/components/Basic/components/AboutCompany/components/CompanyDetail.js
+++ src/components/Company/CompanyMain/components/Basic/components/AboutCompany/components/CompanyDetail.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ReactTooltip from 'react-tooltip';
 import "./CompanyDetail.scss";
 // import { info } from 'node-sass';
 
 const CompanyDetail = (props) => (
   <div>
-    <div className ="labelwithinfo">
-    <div className="label"> {props.label} </div>
-    <div className="info">
-    <FontAwesomeIcon icon={faInfoCircle} />
-    </div>
+    <div className="labelwithinfo">
+      <div className="label"> {props.label} </div>
+      <div className="info" data-tip data-for="infoTip">
+        <FontAwesomeIcon icon={faInfoCircle} />
+      </div>
+      <ReactTooltip id="infoTip" place="top" effect="solid">
+        Property was filled from the ByteCRM database.
+      </ReactTooltip>
     </div>
     <div className="input"> {props.input} </div>
-  </div>
+  </div >
 );
 
 export default CompanyDetail;
