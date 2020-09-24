15e96b5fd937eff70770b08d1493845fb5eddb0c
sherryli.qyl@gmail.com <sherryli.qyl@gmail.com>
2020-09-24 18:57:47 +1000

    modal

--- src/components/Activities/components/Functions/Meeting/components/Header/MeetingPageHeader.js
+++ src/components/Activities/components/Functions/Meeting/components/Header/MeetingPageHeader.js
@@ -1,8 +1,8 @@
 import React, { useContext } from 'react';
 import CreateButton from '../../../../../../Style/Button/Activities/CreateButton';
 import LogButton from '../../../../../../Style/Button/Activities/LogButton';
-import MeetingModal from '../../../../../../Modal/components/Function/Email';
-import { ContactContext } from '../../../../../../Contact/ContactContext';
+import MeetingModal from '../../../../../../Modal/components/Function/Meeting';
+import {ModalContext} from '../../../../../../Modal/components/ModalContext';
 import {meetingHeader} from "./MeetingPageHeader.scss";
 import Modal from '../../../../../../../js/Modal';
 import Grid from '@material-ui/core/Grid';
@@ -11,7 +11,7 @@ import Grid from '@material-ui/core/Grid';
 
 
 const MeetingPageHeader = () => {
-    const onClick= useContext(ContactContext); //receive context
+    const onClick= useContext(ModalContext); //receive context
     const createModal = new Modal('Meeting', 'Meeting',<MeetingModal/>); //create your modal
     const logModal = new Modal('Meeting', 'Meeting',<MeetingModal/>);
     return(
---
+++ src/components/Modal/components/Function/Meeting/components/MeetingInput/MeetingInput.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./MeetingInput.scss";
+import Editor from"../../../../../../Style/Editor/Editor";
+
+const MeetingInput=({
+    children
+})=>(
+
+    <div className="MeetingInput">
+        <Editor/>
+    </div>
+   
+
+);
+
+
+export default MeetingInput;
