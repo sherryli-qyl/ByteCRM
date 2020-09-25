61f80f9088628d2671d0ee0b79eb19238423e4b8
yyr19921203 <123775172@qq.com>
2020-09-26 04:27:46 +1000

    build info icon

--- src/components/InfoPage/services/FormatData.js
+++ src/components/InfoPage/services/FormatData.js
@@ -1,14 +1,15 @@
-function dataInfo(key,title,value) {
+function dataInfo(key,title,value,tip) {
     this.key = key;
     this.title = title;
     this.value = value; 
+    this.tip = tip; 
 }
 
 function FormatData(dataPack,dictionary){
     const dataInfoArray=[];
     for (let i in dictionary){
-        const value = dataPack[dictionary[i].key];    
-        const item = new dataInfo( dictionary[i].key,dictionary[i].title,value);
+        const value = dataPack[dictionary[i].key]; 
+        const item = new dataInfo( dictionary[i].key,dictionary[i].title,value,dictionary[i].tip);
         dataInfoArray.push(item); 
     }
     return dataInfoArray;
