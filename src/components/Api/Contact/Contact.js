e4500fed5e57149364c9817e63d589e1206dfd48
yyr19921203 <123775172@qq.com>
2020-10-02 08:11:57 +1000

    make put method

--- src/components/Api/Contact/Contact.js
+++ src/components/Api/Contact/Contact.js
@@ -1,13 +1,26 @@
 async function GetContact(contactId) {
     const serverUrl = `http://localhost:3000/api/contacts/${contactId}`;
-    const response = await fetch(serverUrl,{
-        method:"GET"
+    const response = await fetch(serverUrl, {
+        method: "GET"
+    });
+    const data = response.json();
+    return data;
+}
+
+async function UpdateContact(contactId, data) {
+    const serverUrl = `http://localhost:3000/api/contacts/${contactId}`;
+    console.log(data);
+    const response = await fetch(serverUrl, {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
     });
     const data = response.json();
     return data;
 }
 
 
-
-export { GetContact };
+export { GetContact, UpdateContact };
 
