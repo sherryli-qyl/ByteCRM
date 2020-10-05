bd84563c966de2ed969741bbb0e4a95ea3b26703
cathy yang <cathy.yf.yang@gmail.com>
2020-10-05 10:21:06 +1000

    Header.jsx change
    Header.jsx change

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,41 +1,21 @@
 import React from 'react';
-import styled from 'styled-components';
 import Public from './components/Public';
 import Private from './components/Private';
-
-const StyledHeader = styled.div`
-  border-bottom: 1px solid #dadada;
-`;
-
-const Container = styled.div`
-  width: 1024px;
-  margin: 0 auto;
-`;
-
-const Layout = styled.div`
-  display: flex;
-`;
-
-const Left = styled.div`
-`;
-
-const Right = styled.div`
-  margin-left: auto;
-`;
+import './Header.scss';
 
 const Header = () => (
-  <StyledHeader>
-    <Container>
-      <Layout>
-        <Left>
+  <div className = "StyledHeader">
+    <div className = "Container">
+      <div className = "Layout">
+        <div className = "Left">
           <Public />
-        </Left>
-        <Right>
+        </div>
+        <div className = "Right">
           <Private />
-        </Right>
-      </Layout>
-    </Container>
-  </StyledHeader>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default Header;
