54957ae8a7f1f509cda1585eebc94bcf0415d77f
yyr19921203 <123775172@qq.com>
2020-10-05 20:40:57 +1100

    build hintBar

--- src/components/SearchContact/components/Dropdown/Dropdown.js
+++ src/components/SearchContact/components/Dropdown/Dropdown.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from '../SearchBar';
 import Select from '../Select';
+import HintBar from '../HintBar';
 import './Dropdown.scss';
 
 
@@ -10,15 +11,48 @@ class Dropdown extends React.Component {
         super(props);
         this.state = {
             checkInput: false,
-            
+            hintMessage: '',
 
+        }
+        this.onChangeInput = this.onChangeInput.bind(this);
+    }
 
+    onChangeInput(text) {
+        let newHint = '';
+        console.log(text.length);
+        if (text.length > 1 && text.length < 3) {
+            newHint = `type ${3 - text.length} more character`;
+            console.log(newHint);
+            this.setState(prevState => {
+                return {
+                    ...prevState,
+                    hintMessage: newHint,
+                    checkInput: true,
+                }
+            })
+        }
+        else if (text.length >= 3) {
+            newHint = 'searching';
+            this.setState(prevState => {
+                return {
+                    ...prevState,
+                    hintMessage: newHint,
+                    checkInput: true,
+                }
+            })
+        }
+        else{
+            this.setState({
+                checkInput:false,
+            })
         }
     }
 
 
     render() {
-        const {showDropdown} = this.props;
+        const { showDropdown } = this.props;
+        const { hintMessage, checkInput } = this.state;
+        console.log(hintMessage);
         let className = "dropdown "
         if (showDropdown) {
             className += "dropdown__active"
@@ -29,12 +63,18 @@ class Dropdown extends React.Component {
                 <div className='dropdown__corner' />
                 <div className='dropdown__inner'>
                     <div className='dropdown__inner__wrapper'>
-                        <SearchBar />
+                        <SearchBar onChange={this.onChangeInput} />
                     </div>
-                    <Select label={'Contacts'}
-                        handleRemoveContact={this.props.handleRemoveContact}
-                        handleAddContact={this.props.handleAddContact}
-                        contactList={this.props.contactList} />
+                    {!checkInput ?
+                        <Select label={'Contacts'}
+                            handleRemoveContact={this.props.handleRemoveContact}
+                            handleAddContact={this.props.handleAddContact}
+                            contactList={this.props.contactList} />
+                        :
+                        <HintBar>
+                            {hintMessage}
+                        </HintBar>
+                    }
                 </div>
             </div>
 
