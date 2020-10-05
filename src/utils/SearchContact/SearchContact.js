e9a07d06f9d2a0a3a88f7dfdf516278d4679f673
yyr19921203 <123775172@qq.com>
2020-10-06 02:08:50 +1100

    fix searchbar btn

--- src/utils/SearchContact/SearchContact.js
+++ src/utils/SearchContact/SearchContact.js
@@ -8,7 +8,6 @@ function SearchContactLocal(contactList,textInput){
     let searchList = [];
     for(let i in contactList){
         if (contactList[i].firstName.toUpperCase().includes(textInput)|| contactList[i].lastName.toUpperCase().includes(textInput)||contactList[i].email.toUpperCase().includes(textInput) ){
-            console.log(textInput);
             // if (!checkDuplicate(searchList,contactList[i])){
                 const searchResult = new SearchResult(contactList[i],true)
                 searchList.push(searchResult);
