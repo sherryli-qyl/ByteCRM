2a5cd33cfa4c4414e2fa0bb4a4560a435676e5e4
cathy yang <cathy.yf.yang@gmail.com>
2020-10-06 22:56:40 +1000

    user change

--- src/components/InfoPage/services/DataPack.js
+++
@@ -1,8 +0,0 @@
-function DataPack(key,title,data,dictionary) {
-    this.key = key;
-    this.title = title;
-    this.data = data; 
-    this.dictionary = dictionary;
-}
-
-export default DataPack;
--- src/js/Cards.js
+++
@@ -1,9 +0,0 @@
-
-
-function Card(date,content) {
-    this.date = date;
-    this.content = content; 
-}
-
-
-export default Card;
