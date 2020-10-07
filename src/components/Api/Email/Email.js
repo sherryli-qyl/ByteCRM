ef8184ed89a76129892eec6bca5596bf3aba2d11
yyr19921203 <123775172@qq.com>
2020-10-07 19:56:06 +1100

    add email Api

---
+++ src/components/Api/Email/Email.js
@@ -0,0 +1,26 @@
+async function GetEmails(contactId) {
+    const serverUrl = `http://localhost:3000/api/emails/${contactId}`;
+    const response = await fetch(serverUrl, {
+        method: "GET"
+    });
+    const data = response.json();
+    return data;
+}
+
+
+async function UpdateContact(contactId, body) {
+    const serverUrl = `http://localhost:3000/api/contacts/${contactId}`;
+    console.log(body);
+    const response = await fetch(serverUrl, {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+    const data = response.json();
+    return data;
+}
+
+
+export {GetEmails, UpdateContact };
