8d4cfe15c69d76e38b5cd454ea98ae295a9bc615
yyr19921203 <123775172@qq.com>
2020-10-11 01:56:28 +1100

    change class Component

--- src/components/ContactSelector/components/Dropdown/Dropdown.js
+++ src/components/ContactSelector/components/Dropdown/Dropdown.js
@@ -6,46 +6,51 @@ import Loading from '../../../Loading';
 import './Dropdown.scss';
 
 
-class Dropdown extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        }
+const Dropdown = ({
+    showDropdown,
+    hintMessage,
+    checkInput,
+    currentList,
+    loading,
+    textInput,
+    enableCleanBtn,
+    handleInputChange,
+    handleCleanInput,
+    handleRemoveContact,
+    handleAddContact
+}) => {
 
+    let className = "dropdown "
+    if (showDropdown) {
+        className += "dropdown__active"
     }
-    render() {
-        const { showDropdown, hintMessage, checkInput, currentList ,loading,oneContact } = this.props
-        let className = "dropdown "
-        if (showDropdown) {
-            className += "dropdown__active"
-        }
-        return (
-            <div className={className}>
-                <div className='dropdown__corner' />
-                <div className='dropdown__inner'>
-                    <div className='dropdown__inner__wrapper'>
-                        <SearchBar textInput={this.props.textInput}
-                                   enableCleanBtn={this.props.enableCleanBtn}
-                                   handleInputChange={this.props.handleInputChange}
-                                   handleCleanInput={this.props.handleCleanInput} />
-                    </div>
-                    {!checkInput ?
-                        <Select label={'Contacts'}
-                                currentList={currentList}
-                                handleRemoveContact={this.props.handleRemoveContact}
-                                handleAddContact={this.props.handleAddContact}/>
-                        :
-                        loading ?
-                            <Loading variant="bar" />
-                            :
-                            <HintBar>
-                                {hintMessage}
-                            </HintBar>
-                    }
+    return (
+        <div className={className}>
+            <div className='dropdown__corner' />
+            <div className='dropdown__inner'>
+                <div className='dropdown__inner__wrapper'>
+                    <SearchBar textInput={textInput}
+                        enableCleanBtn={enableCleanBtn}
+                        handleInputChange={handleInputChange}
+                        handleCleanInput={handleCleanInput} />
                 </div>
+                {!checkInput ?
+                    <Select label={'Contacts'}
+                        currentList={currentList}
+                        handleRemoveContact={handleRemoveContact}
+                        handleAddContact={handleAddContact} />
+                    :
+                    loading ?
+                        <Loading variant="bar" />
+                        :
+                        <HintBar>
+                            {hintMessage}
+                        </HintBar>
+                }
             </div>
-        )
-    }
+        </div>
+    )
 }
+
 
 export default Dropdown;
