b37cf1a60dc8a8c149fde4207e7065d5e6e3b5e8
yyr19921203 <123775172@qq.com>
2020-10-11 01:50:03 +1100

    change component class

--- src/components/ContactSelector/components/Select/components/SelectItem/SelectItem.js
+++ src/components/ContactSelector/components/Select/components/SelectItem/SelectItem.js
@@ -5,64 +5,46 @@ import './SelectItem.scss';
 
 
 
-class SelectItem extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            checked: this.props.checked,
-        }
-        this.onClickCheckbox = this.onClickCheckbox.bind(this);
+const SelectItem = ({
+    contact, 
+    contactID, 
+    disabled, 
+    checked,
+    handleRemoveContact,
+    handleAddContact,
+}) => {
+    let btnClassName = "selectItem__left__checkbox__btn "
+
+    if (disabled) {
+        btnClassName += "selectItem__left__checkbox__btn__disabled"
     }
-
-    onClickCheckbox() {
-        this.setState(prevState => ({
-            checked: !prevState.checked
-        }))
-
-    }
-
-    render() {
-        const { checked } = this.state;
-        const { contact, contactID, disabled } = this.props;
-        console.log("check item" + checked);
-        let btnClassName = "selectItem__left__checkbox__btn "
-
-        if (disabled) {
-            btnClassName += "selectItem__left__checkbox__btn__disabled"
-        }
-        return (
-            <div className='selectItem'>
-                <div className='selectItem__left'>
-                    {checked ?
-                        <div className='selectItem__left__checkbox' >
-                            <button
-                                className={btnClassName}
-                                disabled={disabled}
-                                onClick={event => {
-                                    event.preventDefault();
-                                    this.props.handleRemoveContact(contactID);
-                                    this.onClickCheckbox()
-                                }}>
-                                <FontAwesomeIcon  icon={faCheckSquare} />
-                            </button>
-
-                        </div>
-                        :
-                        <div className='selectItem__left__square'
+    return (
+        <div className='selectItem'>
+            <div className='selectItem__left'>
+                {checked ?
+                    <div className='selectItem__left__checkbox' >
+                        <button
+                            className={btnClassName}
+                            disabled={disabled}
                             onClick={event => {
                                 event.preventDefault();
-                                this.props.handleAddContact(contact);
-                                this.onClickCheckbox()
-                            }} />
-                    }
-                </div>
-                <div className='selectItem__right'>
-                    {`${contact.firstName} ${contact.lastName} (${contact.email})`}
-                </div>
+                                handleRemoveContact(contactID);
+                            }}>
+                            <FontAwesomeIcon icon={faCheckSquare} />
+                        </button>
+                    </div>
+                    :
+                    <div className='selectItem__left__square'
+                        onClick={event => {
+                            event.preventDefault();
+                            handleAddContact(contact);
+                        }} />
+                }
             </div>
-        )
-    }
+            <div className='selectItem__right'>
+                {`${contact.fullName} (${contact.email})`}
+            </div>
+        </div>
+    )
 }
-
-
 export default SelectItem;
