079a7a948a7160ee9dfe66d63cfd12a105a67285
chloecy <caoyue.cufe@gmail.com>
2020-10-10 19:21:38 +1100

    fetch user full name

--- src/components/Activities/components/Functions/Note/components/NoteCardsList/components/NoteCard/NoteCard.js
+++ src/components/Activities/components/Functions/Note/components/NoteCardsList/components/NoteCard/NoteCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CommentBox from './components/CommentBox';
+//import CommentBox from './components/CommentBox';
 import NoteBody from "./components/NoteBody";
 import CreatedBy from "./components/CreatedBy";
 
@@ -7,13 +7,12 @@ import CreatedBy from "./components/CreatedBy";
 class NoteCard extends React.Component {
   constructor(props) {
     super(props);
-    const { _id, content, author, comments } = this.props.card;
+    const { _id, content, comments } = this.props.card;
     this.state = {
       cardId: _id,
       card: this.props.card,
       content,
       comments,
-      author,
     }
     this.onContentChange = this.onContentChange.bind(this);
     this.onCommentChange = this.onCommentChange.bind(this);
@@ -40,21 +39,22 @@ class NoteCard extends React.Component {
   }
 
   render() {
-    const { _id, content, author, comments } = this.props.card;
+    const { cardId, content, comments } = this.props.card;
 
     return (
       <div>
         <NoteBody 
+          cardId={cardId}
           content={content}
           onContentChange={this.onContentChange} 
           />
         <CreatedBy 
-          createdBy={author}
-        />  
-        <CommentBox 
+          createdBy={this.props.card.createdBy.fullName}
+        />
+        {/* <CommentBox 
           comments={comments}
           onCommentChange={this.onCommentChange} 
-        />            
+        />             */}
       </div>
     )
   }
