b49f29928fcd53fbd7df03c905989449c020dd01
yyr19921203 <123775172@qq.com>
2020-10-28 06:09:50 +1100

    fix navlink button

--- src/components/NavigationButton/NavigationButton.js
+++ src/components/NavigationButton/NavigationButton.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
-import styled  from 'styled-components';
 import './NavigationButton.scss';
 
 
-// const NavigationLink = styled(NavLink)`
-//   text-decoration: none;
-//   color: #0091ae;
-
-//   &:hover {
-//     border-bottom: solid 1px #0091ae;
-//   }
-// `;
 
 const NavigationButton = ({
     children,
@@ -26,6 +17,7 @@ const NavigationButton = ({
                     sessionStorage.setItem("id", id);
                 }}>
                 <NavLink
+                    className = "navigationButtonWrapper__btn__link"
                     to={{
                         pathname: path,
                     }}
