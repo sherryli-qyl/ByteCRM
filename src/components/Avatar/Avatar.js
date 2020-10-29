0b29c7fe91bbe8694646a1ff7cef16eff335d913
chloecy <caoyue.cufe@gmail.com>
2020-10-29 23:35:33 +1100

    ./src/components/Avatar/ & ./src/components/Button/
    ./src/components/Avatar/
    ./src/components/Button/

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import './Avatar.scss';
 
-
-
 const Avatar = ({
-    children,
-}) => {
-    
-    return (
-        <div className= "avatar">
-            {children}
-        </div>
-    )
-}
-
+  children,
+}) => (
+  <div className="avatar">
+    {children}
+  </div>
+);
 
 export default Avatar;
