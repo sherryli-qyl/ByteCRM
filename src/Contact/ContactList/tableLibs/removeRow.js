d8f26077d190d0d86b3aa76ae3f7b9deabd415e9
chloecy <caoyue.cufe@gmail.com>
2020-10-29 23:02:51 +1100

    ./src/App/ & ./src/Contact/

--- src/Contact/ContactList/tableLibs/removeRow.js
+++ src/Contact/ContactList/tableLibs/removeRow.js
@@ -1,17 +1,16 @@
-
 const remove = (dataDelete, selectedRow) => {
-    const names = [];
-    for (const item of selectedRow) {
-        names.push(item.name);
+  const names = [];
+  for (const item of selectedRow) {
+    names.push(item.name);
+  }
+  for (let i = 0; i < dataDelete.length;) {
+    if (names.includes(dataDelete[i].name)) {
+      dataDelete.splice(i, 1);
+      continue;
     }
-    for (let i = 0; i < dataDelete.length; ) {
-        if (names.includes(dataDelete[i].name)) {
-            dataDelete.splice(i, 1);
-            continue;
-        }
-        i++;
-    }
-    return dataDelete;
-}
+    i++;
+  }
+  return dataDelete;
+};
 
 export default remove;
--- src/Contact/ContactList/tableLibs/updateRow.js
+++ src/Contact/ContactList/tableLibs/updateRow.js
@@ -1,21 +1,21 @@
-import getDate from "./getDate";
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import getDate from './getDate';
 
 const updateRow = (newData) => {
   newData.createDate = getDate();
-  if (newData.contactOwner === "") {
-    newData.contactOwner = "Unassigned";
+  if (newData.contactOwner === '') {
+    newData.contactOwner = 'Unassigned';
   }
-  if (typeof newData.name !== "object") {
-    const name = newData.name;
+  if (typeof newData.name !== 'object') {
+    const { name } = newData;
     newData.name = (
       <NavLink activeClassName="active" to="/contacts/main">
         {name}
       </NavLink>
     );
   }
-  if (typeof newData.associatedCompany !== "object") {
+  if (typeof newData.associatedCompany !== 'object') {
     const company = newData.associatedCompany;
     newData.associatedCompany = (
       <NavLink activeClassName="active" to="/companies/main">
