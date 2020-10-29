c09fe40af32d95db761d5eefc14485cd4fc028ed
cathy yang <cathy.yf.yang@gmail.com>
2020-10-29 21:51:22 +1000

    Api/User format correction
    Api/User format correction

--- src/components/Api/User/User.js
+++ src/components/Api/User/User.js
@@ -1,13 +1,13 @@
 async function GetAllUsers() {
-  let url = new URL("http://localhost:3000/api/users");
+  const url = new URL('http://localhost:3000/api/users');
   const response = await fetch(url, {
-    method: "GET",
+    method: 'GET',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
   });
   const data = response.json();
   return data;
 }
 
-export{GetAllUsers};
+export { GetAllUsers };
