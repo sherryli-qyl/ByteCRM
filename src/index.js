79c71d2c7dd36ce5356f8ec55bbf7b2154af4bdf
cathy yang <cathy.yf.yang@gmail.com>
2020-10-29 21:24:14 +1000

    utils/auth format correction

--- src/index.js
+++ src/index.js
@@ -7,9 +7,8 @@ import App from './App/App';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App/>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
