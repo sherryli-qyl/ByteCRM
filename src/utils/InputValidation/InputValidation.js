dbb33a4843bfa1e5175cf37fd6a1b11d77f3c952
cathy yang <cathy.yf.yang@gmail.com>
2020-11-01 00:05:27 +1000

    format-error-correction3

--- src/utils/InputValidation/InputValidation.js
+++ src/utils/InputValidation/InputValidation.js
@@ -1,9 +1,9 @@
 function InputValidation(text) {
-    const checkInput = text.replaceAll(' ', '').replaceAll('<br>', '').replaceAll('<p></p>', '');
-    if (checkInput !== '') {
-        return true;
-    }
-    return false;
+  const checkInput = text.replaceAll(' ', '').replaceAll('<br>', '').replaceAll('<p></p>', '');
+  if (checkInput !== '') {
+    return true;
+  }
+  return false;
 }
 
 export default InputValidation;
