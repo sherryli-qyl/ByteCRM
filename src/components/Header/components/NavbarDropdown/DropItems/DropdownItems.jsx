2b89ae562dee444b4180ca1c293db00999ced11e
yyr19921203 <123775172@qq.com>
2020-11-04 22:25:43 +1100

    fix sign out

--- src/components/Header/components/NavbarDropdown/DropItems/DropdownItems.jsx
+++ src/components/Header/components/NavbarDropdown/DropItems/DropdownItems.jsx
@@ -9,6 +9,7 @@ const logout = (history) => {
   removeToken();
   history.push(LOGIN_URL);
 };
+const user = JSON.parse(localStorage.getItem('user'));
 
 const DropdownItems = ({ history }) => {
   if (!isLoggedIn()) return null;
@@ -20,10 +21,10 @@ const DropdownItems = ({ history }) => {
           <img src={Profile} alt="profile" />
         </div>
         <div className="user_name">
-          <p>Li Lei</p>
+          <p>{user.fullName}</p>
         </div>
         <div className="user_email">
-          <p>li.lei@gmail.com</p>
+          <p>{user.email}</p>
         </div>
         <div className="preference">
           <p>Profile & Preferences</p>
@@ -31,9 +32,17 @@ const DropdownItems = ({ history }) => {
       </div>
       <div className="sign_out_wrapper">
         <HeaderNavLink path={LOGIN_URL}>
+          <button onClick={(event)=>{
+            event.preventDefault();
+            localStorage.removeItem('user')
+
+          }}>
+
+        
           <div className="private-text" onClick={() => logout(history)}>
             Sign Out
           </div>
+          </button>
         </HeaderNavLink>
       </div>
     </div>
